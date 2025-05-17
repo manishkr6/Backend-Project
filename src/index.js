a48b68c51b2bfd520bbd9259f4f0548be6e34b89
manish <manishkr9547@gmail.com>
2025-05-17 16:17:17 +0530

    feat: add cookie-parser and cors middleware, implement async error handling, and create ApiError and ApiResponse utility classes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 dotenv.config({
   path: "./env"
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error connecting to MongoDB");
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !!! ", err);
+  });
 
 // import express from "express";
 // const app = express();
---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+// API Error file
+
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Semething went wrong",
+    errors = [],
+    statck = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (statck) {
+      this.stack = statck;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default ApiError;
